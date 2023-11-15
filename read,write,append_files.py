9f34f02ab59f101081f86d7cfd886ec55986b8b9
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-15 13:03:40 -0600

    Update read,write,append_files.py

--- read,write,append_files.py
+++ read,write,append_files.py
@@ -5,5 +5,11 @@ with open(file_path, 'r') as file:
   for line in file:
     print(line)
 
-# writes to the file
+# writes and replaces the data 
 with open(file_path, 'w') as file:
+  file.write("some info")
+
+# appends data to the file
+with open(file_path, "a") as file:
+  file.write("\n now with some more data)
+
